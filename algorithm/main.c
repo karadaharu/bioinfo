991c46d79b68099313de5f7701e5847057a4bda3
Kazumasa <catter121@gmail.com>
2015-05-13 10:01:17 +0900

    Initial Commit

---
+++ algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithm
+//
+//  Created by knekokz on 2015/05/13.
+//  Copyright (c) 2015年 knekokz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
